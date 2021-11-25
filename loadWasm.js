46c77aa5076bdef7d2e1beeaa5e723d461f86383
musixnotmusic <279601306@qq.com>
2021-11-25 17:10:44 +0800

    ...

--- loadWasm.js
+++ loadWasm.js
@@ -1,22 +1,33 @@
-function loadWebAssembly (path, imports = {}) {
-    return fetch(path)
+const imports = {
+  env: {
+    // memoryBase: 0,
+    // memory: new WebAssembly.Memory({ initial: 256 })
+    // tableBase: new WebAssembly.Table({ initial: 0, element: 'anyfunc' })
+  }
+};
+
+export function loadWebAssembly (path) {
+  return fetch(path)
     .then(response => response.arrayBuffer())
     .then(buffer => WebAssembly.compile(buffer))
     .then(module => {
-        imports.env = imports.env || {};
-
-        // alloc memory
-        imports.env.memoryBase = imports.env.memoryBase || 0;
-
-        if (!imports.env.memory) {
-            imports.env.memory = new WebAssembly.Memory({ initial: 256 });
-        }
-
-        imports.env.tableBase = imports.env.tableBase || 0;
-        if (!imports.env.table) {
-            imports.env.table = new WebAssembly.Table({ initial: 0, element: 'anyfunc'});
-        }
-
-        return new WebAssembly.Instance(module, imports);
-    })
+      return new WebAssembly.Instance(module, imports);
+    });
 }
+
+export function loadWebAssembly1 (path, imports = {}) {
+  return fetch(path)
+    .then(response => response.arrayBuffer())
+    .then(buffer => WebAssembly.compile(buffer))
+    .then(module => {
+      return WebAssembly.instantiate(module, imports);
+    });
+}
+
+export function loadWebAssembly2 (path) {
+  return WebAssembly.instantiateStreaming(fetch(path), imports);
+}
+
+window.loadWebAssembly = loadWebAssembly;
+window.loadWebAssembly1 = loadWebAssembly1;
+window.loadWebAssembly2 = loadWebAssembly2;
